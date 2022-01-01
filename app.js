c0bd293e4809f24bb3fcdc7a699cfbf28f9afac4
Arnab Saha <72820989+ArnabTo@users.noreply.github.com>
2022-01-01 23:40:31 +0600

    File Uploaded

---
+++ app.js
@@ -0,0 +1,52 @@
+function no1(){
+    document.form.displayResult.value += '1';
+}
+function no2(){
+    document.form.displayResult.value += '2';
+}
+function no3(){
+    document.form.displayResult.value += '3'
+}
+function no4(){
+    document.form.displayResult.value += '4';
+}
+function no5(){
+    document.form.displayResult.value += '5';
+}
+function no6(){
+    document.form.displayResult.value += '6';
+}
+function no7(){
+    document.form.displayResult.value += '7';
+}
+function no8(){
+    document.form.displayResult.value += '8';
+}
+function no9(){
+    document.form.displayResult.value += '9';
+}
+function nop(){
+    document.form.displayResult.value += '+';
+}
+function nom(){
+    document.form.displayResult.value += '-';
+}
+function noi(){
+    document.form.displayResult.value += '*';
+}
+function nod(){
+    document.form.displayResult.value += '/';
+}
+function noe(){
+    document.form.displayResult.value += '=';
+}
+function noz(){
+    document.form.displayResult.value += '0';
+}
+
+function nodo(){
+    document.form.displayResult.value += '.';
+}
+function noc(){
+    document.form.displayResult.value = '';
+}
